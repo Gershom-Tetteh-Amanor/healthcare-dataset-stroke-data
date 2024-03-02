680101bcdb4dc85f65d82407487a0ee4db91a3a0
Gershom-Tetteh-Amanor <102977349+Gershom-Tetteh-Amanor@users.noreply.github.com>
2024-03-02 11:38:31 +0000

    Update Naive Baye's model deploy.R

--- Naive Baye's model deploy.R
+++ Naive Baye's model deploy.R
@@ -1,6 +1,5 @@
 library(parsnip)
 library(workflows)
-#library(discrim)
 
 ### Creating a vetiver model
 rf_spec <- naive_Bayes() %>%
@@ -11,7 +10,6 @@ rf_wf <- workflow(stroke~., rf_spec)
 
 rf_fit <- rf_wf %>%
   fit(data=train)
-
 
 ### Creating a vetiver model
 library(vetiver)
@@ -24,36 +22,8 @@ model_board <- board_temp(versioned = TRUE)
 model_board %>% vetiver_pin_write(V)
 model_board %>% pin_versions("`stroke prediction`")
 
-
 ### Deploying the model
 library(plumber)
 pr() %>%
-  vetiver_api(V) %>%pr_run(port = 8080)
-
-#library(httr)    
-#set_config(use_proxy(url="10.3.100.207",port=8080))
-
-# authenticates via environment variables:
-vetiver_deploy_rsconnect(model_board,"`stroke prediction`")
-
-vetiver_prepare_docker(model_board, "`stroke prediction`")
-
-
-endpoint <- vetiver_endpoint("https://127.0.0.1:8080/predict")
-endpoint
-
-
-
-library(tibble) 
-new_stroke <- tibble( gender = 'Male', 
-                      age = 67, 
-                      hypertension = '1', 
-                      heart_disease = '1', 
-                      ever_married = 'Yes', 
-                      work_type = 'Private', 
-                      Residence_type = 'Urban',
-                      avg_glucose_level = 340, 
-                      bmi = 48, 
-                      smoking_status = 'smokes' )
-
-predict(endpoint, new_stroke)
+  vetiver_api(V) %>%
+  pr_run(port = 8080)
